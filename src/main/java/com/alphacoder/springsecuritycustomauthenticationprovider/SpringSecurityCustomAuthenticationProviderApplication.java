d0a3276a01c0427e0b6ef9dad60d71dfa019b839
Ramesh Singh <singh.ramesh.eee@gmail.com>
2021-04-21 13:58:48 -0400

    Initial Project setup.

---
+++ src/main/java/com/alphacoder/springsecuritycustomauthenticationprovider/SpringSecurityCustomAuthenticationProviderApplication.java
@@ -0,0 +1,13 @@
+package com.alphacoder.springsecuritycustomauthenticationprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCustomAuthenticationProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCustomAuthenticationProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alphacoder/springsecuritycustomauthenticationprovider/SpringSecurityCustomAuthenticationProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphacoder.springsecuritycustomauthenticationprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCustomAuthenticationProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
